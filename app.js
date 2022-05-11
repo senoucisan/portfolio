04dada5331bdb1d652c329e903cd568f37e9bf0c
senoucisan <senoucisan@gmail.com>
2022-05-11 21:09:40 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,44 @@
+//electing everything that has .section class 
+const sections = document.querySelectorAll('.section');
+
+const sectBtns = document.querySelectorAll('.controls');
+const sectBtn = document.querySelectorAll('.control');
+
+//body
+const allSections = document.querySelector('.main-content');
+
+
+function PageTransition(){
+    //button click active class 
+    for(let i=0;i< sectBtn.length; i++){
+        sectBtn[i].addEventListener('click',function(){
+            let currentBtn = document.querySelectorAll('.active-btn');
+            currentBtn[0].className = currentBtn[0].className.replace('active-btn','');
+            this.className += ' active-btn';
+        })
+    }
+
+    //sections active class 
+    allSections.addEventListener('click', (e) =>{
+        const id = e.target.dataset.id;
+        if(id){
+            //remove selected from the other btns
+            sectBtns.forEach((btn) =>{
+                btn.classList.remove('active')
+            })
+            e.target.classList.add('active')
+
+            //hide other sections 
+            sections.forEach((section)=>{
+                section.classList.remove('active')
+            })
+
+            const element = document .getElementById(id);
+            element.classList.add('active');
+
+
+        }
+    })
+}
+
+PageTransition();
